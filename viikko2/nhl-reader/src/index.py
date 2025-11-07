f39f0f68f418a374b7dcbe36597f8a2a42b22b98
Aura P <aura.pasila@gmail.com>
2025-11-07 19:47:18 +0200

    exercise 2.2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,26 @@
+import requests
 from player import Player
 
 def main():
-    pass
+
+
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    print("Enter nationality")
+    given_nationality = input()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == given_nationality:
+           players.append(player)
+
+    #Kysytty Currechatilta miten voidaan tulostaa sanakirjasta sisältö vain tietyn arvon perusteella, mutta ratkaisu ei hyvä.
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,15 @@
 class Player:
+
+#Kysytty Currechatilta mitä ohjelma tekee ja miten __str___ kannattaa määritellä
+
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals'] 
+        self.team = dict['team'] 
+        self.games = dict['games']
+        self.id = dict['id']
+
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.nationality}, {self.assists}, {self.goals}, {self.team}, {self.games}, {self.id}"
